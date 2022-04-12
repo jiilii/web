45c84e0ae6c2e1e504f4296b1c382e2408460085
jishuli <pgpsys@gmail.com>
2022-04-12 20:16:05 +0800

    first commit.

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function () {
+
+    // let book = document.querySelector('#book');
+    // let ps = book.previousSibling;
+
+    // console.log(ps.innerText);
+
+    // let ps = list.previousElementSibling;
+    // console.log(ps.className);
+    // console.log(ps.innerText);
+
+    // let parent = book.parentElement;
+    // console.log(parent.id);
+
+    // let nav = document.querySelector('#navBox');
+    // let fc = nav.firstElementChild;
+    // console.log(fc.className);
+
+    let book = document.querySelector('#book');
+    let first = book.firstElementChild;
+    console.log(first.nextElementSibling);
+
+
+});
+
